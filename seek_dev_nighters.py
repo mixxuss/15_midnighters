7175204421ec364aad891eef847b7bbaa2d1530d
mixxuss <mixxuss@gmail.com>
2017-02-05 18:18:27 +0300

    Added load attempts func

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,36 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import datetime
+
+def load_attempts(pages=2):
+    url = 'https://devman.org/api/challenges/solution_attempts/'
+    for page in range(1, pages):
+        params = {'page': page}
+        response = requests.get(url, params)
+        records = response.json()['records']
+        for record in records:
+            if record['timestamp']:
+                yield record
+
 
 def get_midnighters():
     pass
 
 if __name__ == '__main__':
-  pass
+    print(load_attempts())
+    for i in load_attempts():
+        print(datetime.fromtimestamp(i['timestamp'], tz=pytz.timezone(i['timezone'])))
+        # print(i)
+
+
+
+    '''params = {}
+    response = requests.get('https://devman.org/api/challenges/solution_attempts/')
+    page = response.json()
+    print(type(page))
+    print(page['records'])
+    def asw():
+        for pa in page['records']:
+            yield pa
+    #for a in asw():
+    #    print(a)'''
