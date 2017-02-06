cbc42a0251aded9fbc89a6b03f1b5bdede47ddd1
mixxuss <mixxuss@gmail.com>
2017-02-06 13:13:46 +0300

    Added func isit_midnighter

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,8 +1,8 @@
 import requests
 import pytz
-from datetime import datetime
+import datetime
 
-def load_attempts(pages=2):
+def load_attempts(pages=3):
     url = 'https://devman.org/api/challenges/solution_attempts/'
     for page in range(1, pages):
         params = {'page': page}
@@ -13,24 +13,15 @@ def load_attempts(pages=2):
                 yield record
 
 
-def get_midnighters():
-    pass
+def isit_midnighter(record):
+    pass_time = datetime.datetime.fromtimestamp(record['timestamp'],
+                                                tz=pytz.timezone(record['timezone']))
+    if pass_time.time() >= datetime.time(hour=0) and pass_time.time() <= datetime.time(hour=1):
+        return record['username']
+
 
 if __name__ == '__main__':
-    print(load_attempts())
-    for i in load_attempts():
-        print(datetime.fromtimestamp(i['timestamp'], tz=pytz.timezone(i['timezone'])))
-        # print(i)
-
-
-
-    '''params = {}
-    response = requests.get('https://devman.org/api/challenges/solution_attempts/')
-    page = response.json()
-    print(type(page))
-    print(page['records'])
-    def asw():
-        for pa in page['records']:
-            yield pa
-    #for a in asw():
-    #    print(a)'''
+    records = load_attempts()
+    for record in records:
+        if isit_midnighter(record):
+            print(isit_midnighter(record))
